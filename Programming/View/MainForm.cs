519236a7bc9cc0835ae865ba5a3b9304cc4507ab
Лев Лунев <levlunev91@gmail.com>
2022-03-02 09:19:28 +0700

    Поправил пространство имен

--- Programming/View/MainForm.cs
+++ Programming/View/MainForm.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Programming.Model;
 
-namespace Programming
+
+namespace Programming.View
 {
     public partial class MainForm : Form
     {
