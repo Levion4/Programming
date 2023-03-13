1fe8bd705eb00c4ff4b0e662a5bb185249ea7e47
Levion4 <levlunev91@gmail.com>
2023-03-13 23:28:57 +0700

    feature(Contacts.sln): создано решение, добавлены папки, написаны классы Contact и Validator, реализована верстка главного окна.

---
+++ src/Contacts/View/Model/Contact.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace View.Model
+{
+    /// <summary>
+    /// Хранит данные о контакте.
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// Ограничение на количество символов в имяни контакта.
+        /// </summary>
+        private readonly int _characterLimit = 150;
+
+        /// <summary>
+        /// Имя контакта.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Номер телефона контакта.
+        /// </summary>
+        private string _phone;
+
+        /// <summary>
+        /// Почта контакта.
+        /// </summary>
+        private string _email;
+
+        /// <summary>
+        /// Возвращает и задает имя контакта.
+        /// Должно состоять только из букв. Длина не более 150 символов.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                Validator.AssertStringContainsOnlyLetters(value, nameof(Name));
+                Validator.AssertValueInRange(value.Length, _characterLimit, nameof(Name));
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает номер телефона контакта.
+        /// Должен начинаться с "+" и содержать 11 цифр.
+        /// </summary>
+        public string Phone
+        {
+            get
+            {
+                return _phone;
+            }
+            set
+            {
+                Validator.IsPhoneValid(value, nameof(Phone));
+                _phone = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает почту контакта.
+        /// Должен быть корректным.
+        /// </summary>
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                Validator.IsEmailValid(value, nameof(Email));                
+                _email = value;
+            }
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        public Contact()
+        {
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        /// <param name="phone">Номер телефона.</param>
+        /// <param name="email">Почта.</param>
+        [JsonConstructor]
+        public Contact(string name, string phone, string email)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+        }
+    }
+}
---
+++ src/Contacts/View/Model/Validator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace View.Model
+{
+    /// <summary>
+    /// Предоставляет методы для валидации.
+    /// </summary>
+    public static class Validator
+    {
+        /// <summary>
+        /// Проверяет, что строка состоит только из букв.
+        /// </summary>
+        /// <param name="value">Проверямая строка.</param>
+        /// <param name="propertyName">Имя свойства или объекта, которое
+        /// подлежит проверке.</param>
+        /// <exception cref="ArgumentException">Возникает, 
+        /// когда строка строка состоит не только из букв.</exception>
+        public static void AssertStringContainsOnlyLetters(string value, string propertyName)
+        {
+            for (var i = 0; i < value.Length; i++)
+            {
+                if (!char.IsLetter(value[i]) && !char.IsWhiteSpace(value[i]))
+                {
+                    throw new ArgumentException($"{propertyName} must contains letters only.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Проверяет, что значение входит в заданный диапазон.
+        /// </summary>
+        /// <param name="value">Проверяемое значение.</param>
+        /// <param name="max">Верхняя граница диапазона.</param>
+        /// <param name="propertyName">Имя свойства или объекта, которое
+        /// подлежит проверке.</param>
+        /// <exception cref="ArgumentException">Возникает, 
+        /// когда значение не входит в заданный диапазон.</exception>
+        public static void AssertValueInRange(int value, int max, string propertyName)
+        {
+            if (value > max)
+            {
+                throw new ArgumentException(
+                    $"The {propertyName} should be in the range up to {max}, " +
+                    $"but was {value}.");
+            }
+        }
+
+        /// <summary>
+        /// Проверяет, что строка является номером телефона.
+        /// </summary>
+        /// <param name="value">Проверяемая строка.</param>
+        /// <param name="propertyName">Имя свойства или объекта, которое
+        /// подлежит проверке.</param>
+        /// <exception cref="ArgumentException">Возникает, 
+        /// когда строка не начинается с "+" или не содержит 11 цифр.</exception>
+        public static void IsPhoneValid(string value, string propertyName)
+        {
+            if (!Regex.IsMatch(value, @"^(\+)[\d]{11}$"))
+            {
+                throw new ArgumentException(
+                    $"The {propertyName} must start with '+' and contain 11 digits.");
+            }
+        }
+
+        /// <summary>
+        /// Проверяет, что строка является почтой.
+        /// </summary>
+        /// <param name="value">Проверяемая строка.</param>
+        /// <param name="propertyName">Имя свойства или объекта, которое
+        /// подлежит проверке.</param>
+        /// <exception cref="ArgumentException">Возникает,
+        /// когда строка не явлется почтой.</exception>
+        public static void IsEmailValid(string value, string propertyName)
+        {
+            try
+            {
+                MailAddress mailAddress = new MailAddress(value);
+            }
+            catch
+            {
+                throw new ArgumentException($"The {propertyName} is incorrect.");
+            }
+        }
+    }
+}
